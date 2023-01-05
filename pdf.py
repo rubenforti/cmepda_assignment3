a7f6ad90b7911edcf7e69a04dbd6e7a8c35e6e8b
rubenforti <rubenforti27@gmail.com>
2023-01-05 19:32:20 +0100

    True final commit

--- pdf.py
+++ pdf.py
@@ -108,28 +108,23 @@ class TestFunctions(unittest.TestCase):
     Class for unit testing
     """
 
-    def _x(self):
-        return np.linspace(0, 1, 1000)
-
-    def _y(self):
-        return 4*self._x**3
-
-    def _p(self):
-        return ProbabilityDensityFunction(self._x, self._y)
-
     def test_cdf(self):
         """ Unit testing of CDF """
-        y_test = self._x**4
-        P = self._p()
-        y_cdf = P.cdf(self._x)
+        x_t1 = np.linspace(0, 1, 1000)
+        y_t1 = 4*(x_t1**3)
+        P = ProbabilityDensityFunction(x_t1, y_t1)
+        y_test = x_t1**4
+        y_cdf = P.cdf(x_t1)
         for i in range(0, 1000):
             self.assertAlmostEqual(y_test[i], y_cdf[i])
 
     def test_ppf(self):
         """ Unit testing of PPF """
-        y_test = self._x**(1/4.)
-        P = self._p()
-        y_ppf = P.ppf(self._x)
+        x_t2 = np.linspace(0, 1, 1000)
+        y_t2 = 4*(x_t2**3)
+        P = ProbabilityDensityFunction(x_t2, y_t2)
+        y_test = x_t2**(1/4)
+        y_ppf = P.ppf(x_t2)
         for i in range(0, 1000):
             self.assertAlmostEqual(y_ppf[i], y_test[i])
 
